bfa60c4f4f4f219a7b426c5b4bc58f012ca6bc74
Abelino <adv.blvd@gmail.com>
2023-11-23 16:24:36 -0600

    Se añadieron nuevos queries a este commit 2

--- etl_dms.py
+++ etl_dms.py
@@ -15,9 +15,18 @@ database = 'bi_db'
 engine = create_engine(f'mssql+pyodbc://{user}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server')
 conn = engine.connect()
 
-
     
 # %%
-query = "SELECT TOP 1000 customerid, genderid FROM dbo.customers;"
+query = 'SELECT * FROM dbo.customers;'
 df = pd.read_sql(query, conn)
 df.head()
+
+# %%
+path = 'data\data_dms_20180101.csv'
+df_raw_data = pd.read_csv(path)
+
+
+# %%
+query_ship_modes = 'SELECT * FROM dbo.shipModes'
+df_raw_data = pd.read_sql(query_ship_modes, engine)
+# %%
